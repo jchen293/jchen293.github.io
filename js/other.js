8109a4f09c9a18f01a073e72ce49899898ad4058
jchen293 <jenny0418kobe@gmail.com>
2020-02-19 02:51:34 -0500

    resume updated

--- js/other.js
+++ js/other.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
-  $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-  var hash = this.hash;
-    scrollTop: $(hash).offset().top
-  }, 900, function(){
-    window.location.hash = hash;
-    });
-  });
-})
+$(document).ready(function() {
+  $(".navbar a, footer a[href='#myPage']").on(
+    "click",
+    function(event) {
+      var hash = this.hash;
+      scrollTop: $(hash).offset().top;
+    },
+    900,
+    function() {
+      window.location.hash = hash;
+    }
+  );
+});
--- js/timer.js
+++ js/timer.js
@@ -1,21 +1,20 @@
 var countDownDate = new Date("Aug 9, 2018 12:37:25").getTime();
 
 var x = setInterval(function() {
+  var now = new Date().getTime();
 
-    var now = new Date().getTime();
+  var distance = countDownDate - now;
 
-    var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  //   document.getElementById("demo").innerHTML =
+  //     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("demo").innerHTML = "Enter Website";
-    }
+  //   if (distance < 0) {
+  //     clearInterval(x);
+  //     document.getElementById("demo").innerHTML = "Enter Website";
+  //   }
 }, 1000);
