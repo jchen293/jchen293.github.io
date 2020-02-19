05a17de9351dff74f2517cca4c7c9603ecbc26f8
jchen293 <jenny0418kobe@gmail.com>
2020-02-19 02:56:44 -0500

    bug fixed

--- js/timer.js
+++ js/timer.js
@@ -9,12 +9,4 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  //   document.getElementById("demo").innerHTML =
-  //     days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-  //   if (distance < 0) {
-  //     clearInterval(x);
-  //     document.getElementById("demo").innerHTML = "Enter Website";
-  //   }
 }, 1000);
